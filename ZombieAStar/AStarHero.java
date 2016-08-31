d650c375acd9dcb76b6a74467a1cf32b0f4b9e06
Matthew <matthew.whitehead@coloradocollege.edu>
2016-08-31 14:01:46 -0600

    Cleanup astar hero

--- ZombieAStar/AStarHero.java
+++ ZombieAStar/AStarHero.java
@@ -3,58 +3,56 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-
-
+       
+       
 class AStarNode implements Comparable {
-
+    
     public Location loc = null;
     public double cost = 0.0;
     public int move_number = 0;
-
+    
     public AStarNode(Location curr_loc, Location start, Location end, int _move_number) {
         move_number = _move_number;
-        loc = curr_loc;
-        double startDist = loc.distanceTo(start);
-        double endDist = loc.distanceTo(end);
+        double endDist = curr_loc.distanceTo(end);
         cost = endDist + move_number;
     }
-
+    
     public int compareTo(Object otherNode) {
         AStarNode other = (AStarNode) otherNode;
         return new Double(cost).compareTo(new Double(other.cost));
     }
-
+    
     public String toString() {
         return "ASTARNODE: " + loc + "\t" + cost + "\n";
     }
-
-
+    
+       
 }
 public class AStarHero extends Hero {
-
+   
     public AStarHero() {
         // init junk here
     }
-
+    
     public void findPath(ZombieEscape game) {
-
+        
         PriorityQueue<AStarNode> q = new PriorityQueue<>();
         HashMap<Location, Boolean> visited = new HashMap<>();
-
+        
         Location start = game.state.hero_location;
         Location end = game.state.zombie_location;
-
+        
         AStarNode first = new AStarNode(start, start, end, 0);
         q.add(first);
-
+        
         while (!game.foundGoal()) {
-
+            
             AStarNode curr = q.poll();
             visited.put(curr.loc, true);
             game.state.hero_location = curr.loc;
-
+            
             ArrayList<Location> legal_moves = game.getLegalMoves(curr.loc);
-
+            
             for (Location loc : legal_moves) {
                 if (!visited.containsKey(loc)) {
                     AStarNode currA = new AStarNode(loc, start, end, curr.move_number + 1);
@@ -62,9 +60,9 @@ public class AStarHero extends Hero {
                     visited.put(loc, true);
                 }
             }
-
+            
             System.out.println(q);
-
+            
             try {
                 Thread.sleep(50);
                 game.repaint();
@@ -73,6 +71,6 @@ public class AStarHero extends Hero {
                 System.out.println("Execution interrupted!");
             }
             game.move_number++;
-        }
+        }   
     }
 }
--- ZombieAStar/GreedyHero.java
+++ ZombieAStar/GreedyHero.java
@@ -2,36 +2,40 @@ import java.util.Random;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.ArrayList;
-
-
+       
+       
 public class GreedyHero extends Hero {
+   
+    public GreedyHero() {
+        // init junk here
+    }
 
     public void findPath(ZombieEscape game) {
-
+        
          while (!game.foundGoal()) {
-
-            ArrayList<Location> legal = game.getLegalMoves(game.state.hero_location);
-
-            double shortest = Double.MAX_VALUE;
-            Location best_move = legal.get(0);
-            for (Location move : legal) {
-              double dist = move.distanceTo(game.state.zombie_location);
-              if (dist < shortest) {
-                shortest = dist;
-                best_move = move;
-              }
+            ArrayList<Location> legal_moves = game.getLegalMoves(game.state.hero_location);
+            
+            Location best = legal_moves.get(0);
+            double min_dist = best.distanceTo(game.state.zombie_location);
+            
+            for (Location loc : legal_moves) {
+                double dist = loc.distanceTo(game.state.zombie_location);
+                if (dist < min_dist) {
+                    min_dist = dist;
+                    best = loc;
+                }
             }
-
-            System.out.println("MOVING: " + best_move);
-            game.state.hero_location = best_move;
+            
+            System.out.println("MOVING: " + best);
+            game.state.hero_location = best;
             try {
-                Thread.sleep(250);
+                Thread.sleep(1000);
                 game.repaint();
                 System.out.println("TICK");
             } catch (InterruptedException e) {
                 System.out.println("Execution interrupted!");
             }
             game.move_number++;
-        }
+        }   
     }
 }
